f1cdac7b5b5a8991572a0198661e64c007dc0799
ChantaClown <pablo.chantada@udc.es>
2024-02-14 21:11:12 +0100

    PARTE 2

---
+++ main.jl
@@ -0,0 +1,123 @@
+# Numero Maximo de capas -> 2
+# Usar funcion Dense para los Perceptrones Multicapa:
+#   Numero de entradas
+#   Salidas
+#   Funcion de transferencia
+# Transponer las matrices creadas
+# Usar Float32 casi siempre, para las practicas usarlo siempre
+
+using Flux;
+using Flux.Losses;
+using FileIO;
+using DelimitedFiles;
+
+# PARTE 1
+# --------------------------------------------------------------------------
+
+function oneHotEncoding(feature::AbstractArray{<:Any,1}, classes::AbstractArray{<:Any,1})
+
+    numClasses = length(classes);
+    @assert(numClasses>1);
+    if numClasses==2
+        # Si solo hay dos clases, se devuelve una matriz con una columna.
+        one_col_matrix = reshape(feature.==classes[1], :, 1);
+        return one_col_matrix
+    else
+        # Si hay mas de dos clases se devuelve una matriz con una columna por clase.
+        oneHot = Array{Bool,2}(undef, length(feature), numClasses);
+        for numClass = 1:numClasses
+            oneHot[:,numClass] .= (feature.==classes[numClass]);
+        end;
+        matriz = oneHot;
+    end;
+    return matriz
+end;
+
+function oneHotEncoding(feature::AbstractArray{<:Any,1})
+    classes = unique(feature)
+    @assert(numClasses>1);
+    if numClasses==2
+        # Si solo hay dos clases, se devuelve una matriz con una columna.
+        one_col_matrix = reshape(feature.==classes[1], :, 1);
+        return one_col_matrix
+    else
+        # Si hay mas de dos clases se devuelve una matriz con una columna por clase.
+        oneHot = Array{Bool,2}(undef, length(feature), numClasses);
+        for numClass = 1:numClasses
+            oneHot[:,numClass] .= (feature.==classes[numClass]);
+        end;
+        matriz = oneHot;
+    end;
+    return matriz
+end;
+
+function oneHotEncoding(feature::AbstractArray{Bool,1})
+    one_col_matrix = reshape(feature, :, 1)
+    return one_col_matrix
+end;
+
+# PARTE 2
+# --------------------------------------------------------------------------
+
+function calculateMinMaxNormalizationParameters(dataset::AbstractArray{<:Real,2}) # vectores de ints y floats
+    # recibe una matriz
+    # duvuelve una tupla con
+        # matriz de 1 fila -> min de cada columna
+        # matriz de 1 fila -> max de cada columna
+    min_col_value = minimum(dataset, dims=1)
+    max_col_value = maximum(dataset, dims=1)
+    return (min_col_value, max_col_value)
+end;
+
+function calculateZeroMeanNormalizationParameters(dataset::AbstractArray{<:Real,2}) 
+end;
+
+#=
+# PARTE 3
+function normalizeMinMax!(dataset::AbstractArray{<:Real,2}, normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+
+function normalizeMinMax( dataset::AbstractArray{<:Real,2}, normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+
+function normalizeMinMax!(dataset::AbstractArray{<:Real,2})
+
+function normalizeMinMax( dataset::AbstractArray{<:Real,2})
+
+# PARTE 4
+function normalizeZeroMean!(dataset::AbstractArray{<:Real,2},normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+
+function normalizeZeroMean!(dataset::AbstractArray{<:Real,2})
+
+function normalizeZeroMean( dataset::AbstractArray{<:Real,2},normalizationParameters::NTuple{2, AbstractArray{<:Real,2}})
+
+function normalizeZeroMean( dataset::AbstractArray{<:Real,2})
+
+# PARTE 5
+function classifyOutputs(outputs::AbstractArray{<:Real,1}; threshold::Real=0.5)
+
+function classifyOutputs(outputs::AbstractArray{<:Real,2}; threshold::Real=0.5)
+
+# PARTE 6
+function accuracy(outputs::AbstractArray{Bool,1}, targets::AbstractArray{Bool,1})
+
+function accuracy(outputs::AbstractArray{Bool,2}, targets::AbstractArray{Bool,2})
+
+function accuracy(outputs::AbstractArray{<:Real,1}, targets::AbstractArray{Bool,1}; threshold::Real=0.5)
+
+function accuracy(outputs::AbstractArray{<:Real,2}, targets::AbstractArray{Bool,2}; threshold::Real=0.5)
+
+# BUCLE WHILE SOLO AQUI PARA ITERAR EL ENTRENAMIENTO
+# EL OPTIMIZADOR SE CREA FUERA DEL BUCLE
+# PARTE 7
+function buildClassANN(numInputs::Int, topology::AbstractArray{<:Int,1}, numOutputs::Int;
+    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology))) 
+
+#PARTE 8
+function trainClassANN(topology::AbstractArray{<:Int,1},
+    dataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}};
+    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
+    maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01)
+
+function trainClassANN(topology::AbstractArray{<:Int,1},
+    (inputs, targets)::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,1}};
+    transferFunctions::AbstractArray{<:Function,1}=fill(σ, length(topology)),
+    maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01) =#
--- pruebas_unitarias.jl
+++ pruebas_unitarias.jl
@@ -1,8 +1,4 @@
-
-include("prac1.jl")
-
-
-#¿¿¿¿pruebas unitarias??????
+#=
 # Crear un vector de ejemplo con valores booleanos
 feature_bool = [true, false, true, true, false]
 
@@ -18,3 +14,21 @@ feature_clases = ["A", "B", "A", "C", "B", "C"]
 matriz_resultante = oneHotEncoding(feature_clases, unique(feature_clases))
 println("\nMatriz resultante para la función oneHotEncoding con dos parámetros:")
 println(matriz_resultante)
+=#
+
+include("main.jl")
+
+dataset = readdlm("iris.data",',');
+inputs = dataset[:,1:4];
+inputs = convert(Array{Float32,2},inputs);
+targets = dataset[:,5];
+
+# PARTE 1
+# --------------------------------------------------------------------------
+
+# PARTE 2
+# --------------------------------------------------------------------------
+
+min_val, max_val = calculateMinMaxNormalizationParameters(inputs)
+println("Minimum values per column: ", min_val)
+println("Maximum values per column: ", max_val)
